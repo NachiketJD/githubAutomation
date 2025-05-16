8a89a2f6ceafaef7a25cac55d8cec9eed9804cc0
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-05-17 00:08:43 +0530

    Add: 'funct'

--- auto.py
+++ auto.py
@@ -1,9 +1,23 @@
 import subprocess
 
-input_code = """function hello() {
-  console.log("Hello World");
+input_code = """function greet(name) {
+  const message = `Hello, ${name}! Welcome to the system.`;
+  console.log(message);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function isEven(n) {
+  return n % 2 === 0;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i, isEven(i) ? "Even" : "Odd");
 }
 """
+
 
 output_file = "main.js"
 with open(output_file, "w") as f:
--- main.js
+++ main.js
@@ -1,3 +1,1 @@
-function hello() {
-  console.log("Hello World");
-}
+funct
