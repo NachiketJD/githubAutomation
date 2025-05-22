f04e82fa8bb5d6a9bd2cab22901d0a4785511b87
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-05-22 12:06:11 +0530

    Add: 'le)\n '

--- main.py
+++ main.py
@@ -21,4 +21,5 @@ def extract_text(file: UploadFile):
     if file.filename.endswith(".txt"):
         return file.file.read().decode()
     elif file.filename.endswith(".docx"):
-        doc = Document(file.fi
+        doc = Document(file.file)
+ 
