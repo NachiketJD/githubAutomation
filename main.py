370cb64bbab6f56a597dbcc24a931d18b545b4b6
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-09 12:53:39 +0530

    Add: 'from '

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from fastapi import FastAPI, File, Form, UploadFile
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-import os, tempfile
-from git import Repo
-from docx import Document
-import fitz  # PyMuPDF
-
-app = FastAPI()
-
-# Enable CORS for frontend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
- 
+from 
