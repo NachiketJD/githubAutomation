e86318ffb6dab75751ff693a4728faabb638b962
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-07-07 23:58:27 +0530

    Add: 'eware'

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddl
+from fastapi.middleware.cors import CORSMiddleware
